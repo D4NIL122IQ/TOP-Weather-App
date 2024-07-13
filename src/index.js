5435d3f339209e854bd2e0ab196631f6cc30335e
Danil gdj <danilguidjou7@gmail.com>
2024-07-13 22:59:33 +0100

    meteo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import windImg from './assets/vent.png'
 import sunriseImg from './assets/sunrise.png'
 import sunsetImg from './assets/sunset.png'
 import ressentieImg from './assets/ressentie.png'
+import sunsetrise from './assets/sunsetndrise.png'
+import avgTemp from './assets/avgtemp.png'
 
 const section = document.querySelector('section')
 const title = document.querySelector('.loc')
@@ -25,7 +27,7 @@ const sunsetTime = document.querySelector('#sunsettime')
 const sunriseTime = document.querySelector('#sunrisetime')
 const minmaxTexte = document.querySelector('.minmaxTemp')
 
-const CardContainer = document.querySelector('.displayCard')
+const CardContainer = document.querySelector('.displaycard')
 
 
 document.querySelector('#rechercher').src = imgFind
@@ -55,7 +57,7 @@ if (navigator.geolocation) {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
         getTempInstant(String(latitude) +','+String(longitude))
-
+        
       },
       function(error) {
         getTempInstant('bejaia')
@@ -89,6 +91,7 @@ async function getTempInstant(villeName){
     })
     getSunTime(villeName)
     getMinMaxTempToday(villeName)
+    
 }
 
 async function getSunTime(villeName) {
@@ -110,11 +113,81 @@ async function getMinMaxTempToday(villeName){
     })
 }
 
+function setupForecastMeteo(themeteo){
+    let div = document.createElement('div')
+    let date = document.createElement('h4')
+    let minmaxText = document.createElement('h4')
+    let avgTempe = document.createElement('h4')
+    let avgHumidity = document.createElement('h4')
+    let sunrise_sunsetTime = document.createElement('h4')
+
+    let t2 = document.createElement('h4')
+    let t3 = document.createElement('h4')
+    let div1 = document.createElement('div')
+    let div2 = document.createElement('div')
+    let div3 = document.createElement('div')
+    let div4 = document.createElement('div')
+    let img1  = document.createElement('img')
+    let img2 = document.createElement('img')
+    let img3 = document.createElement('img')
+    let img4 = document.createElement('img')
+
+    div1.className = 'infoDisplay'
+    div2.className = 'infoDisplay'
+    div3.className = 'infoDisplay'
+    div4.className = 'infoDisplay'
+
+    div.className = 'card'
+    date.textContent = themeteo.date
+    img1.src = minmaxImg
+    img1.width = 40
+    minmaxText.textContent = `${themeteo.day.mintemp_c}°C - ${themeteo.day.maxtemp_c}°C`
+    div1.appendChild(img1)
+    div1.appendChild(minmaxText)
+
+    img2.width = 40
+    img2.src = imgHumidity
+    t2.textContent = 'Humidité moyenne : '
+    avgHumidity.textContent = `${themeteo.day.avghumidity}%`
+    div2.appendChild(img2)
+    div2.appendChild(t2)
+    div2.appendChild(avgHumidity)
+
+    img3.width = 40
+    img3.src = avgTemp
+    t3.textContent = 'Temperature moyenne :'
+    avgTempe.textContent = `${themeteo.day.avgtemp_c}°C`
+    div3.appendChild(img3)
+    div3.appendChild(t3)
+    div3.appendChild(avgTempe)
+
+    img4.src = sunsetrise
+    img4.width = 40
+    sunrise_sunsetTime.textContent = `${themeteo.astro.sunrise} - ${themeteo.astro.sunset}`
+    div4.appendChild(img4)
+    div4.appendChild(sunrise_sunsetTime)
+
+    div.appendChild(date)
+    div.appendChild(div1)
+    div.appendChild(div3)
+    div.appendChild(div2)
+    div.appendChild(div4)
+
+
+    CardContainer.appendChild(div)
+}
+
 async function getTempFor4Days (villeName){
-    let URL = `https://api.weatherapi.com/v1/forecast.json?key=14195a369dfb44a8bc803821240707&lang=fr&q=${villeName}&day=4`
+    let URL = `https://api.weatherapi.com/v1/forecast.json?key=14195a369dfb44a8bc803821240707&lang=fr&q=${villeName}&days=5`
 
     const rep = await fetch(URL)
-    rep.json().then()
+    rep.json().then(function(rep){
+        console.log(rep)
+       setupForecastMeteo(rep.forecast.forecastday[1])
+       setupForecastMeteo(rep.forecast.forecastday[2])
+       setupForecastMeteo(rep.forecast.forecastday[3])
+       setupForecastMeteo(rep.forecast.forecastday[4])
+    })
 }
 
 document.querySelector("img").addEventListener('click' , ()=>{
@@ -123,3 +196,4 @@ document.querySelector("img").addEventListener('click' , ()=>{
    
 })
 
+getTempFor4Days('bejaia')
